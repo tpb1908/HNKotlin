54a4e6d78e14c5c8cb17015192a41051b58dd0eb
tpb1908 <theo1908phone@gmail.com>
2017-07-10 21:39:36 +0100

    Add DBModule.

---
+++ app/src/main/java/com/tpb/hnk/dagger/module/DBModule.kt
@@ -0,0 +1,32 @@
+package com.tpb.hnk.dagger.module
+
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.migration.Migration
+import android.content.Context
+import com.tpb.hnk.data.database.Database
+import com.tpb.hnk.data.database.ItemDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * Created by theo on 10/07/17.
+ */
+@Module class DBModule(val context: Context, val dbName: String, val migrations: Array<Migration>) {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(): Database {
+        return Room.databaseBuilder(context, Database::class.java, dbName)
+                .addMigrations(*migrations)
+                .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideItemDao(database: Database): ItemDao {
+        return database.itemDao()
+    }
+
+}
